f62daa4e54a6aa2a7fd898b9e14509e2769b4331
jamabde03 <jameswalkerabdey@gmail.com>
2023-06-10 15:49:42 -0700

    initial commit

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    public float speed = 3;
+    public bool isGrounded;
+    public float deaths;
+    public Vector2 startPos;
+    public enum gameMode {Cube, Ship, Wave, UFO, Ball, Spider}
+    public gameMode currentGM;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentGM = gameMode.Cube;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        movePlayer();
+        
+    }
+
+    void Jump()
+    {
+        if(Input.GetMouseButton(0) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, (speed + 5) * rb.gravityScale/2);
+            isGrounded = false;
+        }
+    }
+
+    void Fly()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            rb.AddForce(new Vector2(0, 3 * (rb.gravityScale/2)));
+        }
+    }
+
+    void Wave()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 6);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -6);
+        }
+    }
+
+    void movePlayer()
+    {
+        if (currentGM == gameMode.Cube)
+        {
+            Jump();
+        }
+        else if (currentGM == gameMode.Ship)
+        {
+            Fly();
+        }
+        else if (currentGM == gameMode.Wave)
+        {
+            Wave();
+        }else if(currentGM == gameMode.Ball)
+        {
+            if(Input.GetMouseButtonDown(0) && isGrounded)
+            {
+                rb.gravityScale *= -1;
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("ground"))
+        {
+            if (transform.position.x + 0.49f < collision.gameObject.transform.position.x - collision.gameObject.transform.localScale.x/2)
+            {
+                deaths++;
+                transform.position = startPos;
+                currentGM = gameMode.Cube;
+                rb.gravityScale = 2;
+                isGrounded = true;
+            }
+            isGrounded = true;
+            if(currentGM == gameMode.Wave)
+            {
+                deaths++;
+                transform.position = startPos;
+                currentGM = gameMode.Cube;
+                rb.gravityScale = 2;
+            }
+        }
+
+        if(collision.gameObject.CompareTag("spike"))
+        {
+            deaths++;
+            transform.position = startPos;
+            currentGM = gameMode.Cube;
+            rb.gravityScale = 2;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = false;
+        }
+
+        if (collision.gameObject.CompareTag("yellowOrb"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("test");
+        if (collision.gameObject.CompareTag("shipPortal"))
+        {
+            currentGM = gameMode.Ship;
+            rb.gravityScale = 2;
+        }
+
+        if (collision.gameObject.CompareTag("cubePortal"))
+        {
+            currentGM = gameMode.Cube;
+            rb.gravityScale = 2;
+        }
+
+        if (collision.gameObject.CompareTag("gravityPortal"))
+        {
+            rb.gravityScale *= -1;
+        }
+
+        if (collision.gameObject.CompareTag("wavePortal"))
+        {
+            rb.gravityScale = 0;
+            currentGM = gameMode.Wave;
+        }
+
+        if (collision.gameObject.CompareTag("ballPortal"))
+        {
+            currentGM = gameMode.Ball;
+            rb.gravityScale = 2;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("yellowOrb"))
+        {
+            isGrounded = true;
+        }
+        if (collision.gameObject.CompareTag("blueOrb"))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.gravityScale *= -1;
+            }
+        }
+    }
+}
